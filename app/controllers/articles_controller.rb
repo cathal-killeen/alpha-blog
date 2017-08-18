1d6b1cadaf99a3eb7ae2b1439faa6f5cd9419ff9
Cathal Killeen <cathalkilleen+cloud9@gmail.com>
2017-08-18 12:15:36 +0000

    Add 'edit', 'show' and 'update' Article

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,46 @@
+class ArticlesController < ApplicationController
+    def index
+        @articles = Article.all
+    end
+    
+    def new
+        @article = Article.new
+    end
+    
+    def edit
+        @article = Article.find(params[:id])
+    end 
+    
+    def create
+        # create new article object
+        @article = Article.new(article_params)
+        
+        # attempt save, redirect based on outcome
+        if @article.save
+            flash[:notice] = "Article was successfully created"
+            redirect_to article_path(@article) 
+        else 
+            # redirect to articles/new route
+            render 'new'                                
+        end
+    end
+    
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article was successfully updated"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        end
+    end
+    
+    def show
+        @article = Article.find(params[:id]) 
+    end
+    
+    private
+    def article_params
+        params.require(:article).permit(:title, :description)    
+    end
+end
